c9a26e0a992c0cc5af801c6f5df8f99be3f9a098
Vladimir Krasnov <v@krsnv.ru>
2018-08-09 18:50:22 +0300

    ENV-PREPARE Добавил пример endpoint, который выдает версию приложения

--- marketplace.app/marketplace/__init__.py
+++ marketplace.app/marketplace/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/version')
+def version():
+    return jsonify(version=1.0)
 
 
 if __name__ == '__main__':
