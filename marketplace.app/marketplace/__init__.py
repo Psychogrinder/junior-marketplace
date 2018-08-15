5fd96957a808721e38bc222b52a908cc5dbd0026
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-08-15 18:12:01 +0300

    HF удален лишний def index()

--- marketplace.app/marketplace/__init__.py
+++ marketplace.app/marketplace/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 from flask_assets import Environment, Bundle
-from config import Config
+from marketplace.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_marshmallow import Marshmallow
 import cssmin
-from marketplace import models, views
+
 
 app = Flask(__name__)
 assets = Environment(app)
@@ -17,10 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+from marketplace import models, views
 
 
 css = Bundle('style/authorization.css','style/header.css', 'style/footer.css', 'style/catalog.css',
@@ -30,4 +27,3 @@ assets.register('css_all', css)
 
 if __name__=='__main__':
     app.run(port=8000)
-
