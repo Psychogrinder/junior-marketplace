ecc72fcca75d4e36709b95fc632100f3c4d3cef4
IrinaZorkova <irinanazorkova@gmail.com>
2018-08-15 17:00:17 +0300

    ISSUE-58 заглушка главной

--- marketplace.app/marketplace/__init__.py
+++ marketplace.app/marketplace/__init__.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_assets import Environment, Bundle
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_marshmallow import Marshmallow
+import cssmin
 
 app = Flask(__name__)
+assets = Environment(app)
 app.config.from_object(Config)
 ma = Marshmallow(app)
 api = Api(app)
@@ -13,3 +16,16 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 db.init_app(app)
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+css = Bundle('style/authorization.css','style/header.css', 'style/footer.css', 'style/catalog.css',
+             filters=['cssmin'], output='bundle.min.css')
+assets.register('css_all', css)
+
+
+if __name__=='__main__':
+    app.run(port=8000)
