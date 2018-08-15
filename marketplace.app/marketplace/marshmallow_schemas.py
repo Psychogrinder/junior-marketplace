a4269c0661232a03ea7c648ae6f524553098f3c2
Egor Galkin <egor.galkin99@yandex.ru>
2018-08-15 15:37:11 +0300

    ISSUE-56 Added deserialization oportunity to marshmallow_schemas.py

--- marketplace.app/marketplace/marshmallow_schemas.py
+++ marketplace.app/marketplace/marshmallow_schemas.py
@@ -1,6 +1,6 @@
-from marketplace.models import Order, Product, Consumer, Producer, Category
-from marketplace import ma
-from marshmallow import fields
+from app.models import Order, Product, Consumer, Producer, Category
+from app import ma
+from marshmallow import fields, post_load
 from marshmallow_sqlalchemy import ModelConverter
 from sqlalchemy.dialects.postgresql import MONEY
 
@@ -17,11 +17,19 @@ class OrderSchema(ma.ModelSchema):
         model = Order
         model_converter = PostgresMoneyConverter
 
+        @post_load
+        def create_order(self, data):
+            return Order(**data)
+
 
 class ProductSchema(ma.ModelSchema):
     class Meta:
         model = Product
         model_converter = PostgresMoneyConverter
+
+        @post_load
+        def create_product(self, data):
+            return Product(**data)
 
 
 class ConsumerSchema(ma.ModelSchema):
@@ -29,14 +37,26 @@ class ConsumerSchema(ma.ModelSchema):
         model = Consumer
         fields = ('id', 'email', 'name', 'person_to_contact', 'description', 'phone_number', 'address')
 
+        @post_load
+        def create_consumer(self, data):
+            return Order(**data)
+
 
 class ProducerSchema(ma.ModelSchema):
     class Meta:
         model = Producer
         fields = ('id', 'email', 'first_name', 'patronymic', 'last_name', 'phone_number', 'address')
 
+        @post_load
+        def create_producer(self, data):
+            return Producer(**data)
+
 
 class CategorySchema(ma.ModelSchema):
     class Meta:
         model = Category
+        
+        @post_load
+        def create_category(self, data):
+            return Category(**data)
 
