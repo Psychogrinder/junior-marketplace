446a128c142d8229fefee90e9337542bc4e5a3de
Roman Melnychuk <jenikuse@gmail.com>
2018-08-20 16:44:44 +0300

    ISSUE-38 добавил проверку категорий по запросу

---
+++ marketplace.app/marketplace/smoke_tests.py
@@ -0,0 +1,63 @@
+#import sys
+#sys.path.insert(0, '/home/elama/dev-tools/Projects/junior-marketplace/marketplace.app/marketplace')
+
+import unittest
+from urllib.request import Request, urlopen
+from selenium import webdriver
+
+
+#подменяет '<name_category>' на имя существующей категории для домтупа по ссылке
+def addCategoryLinks(link, category):
+    return link.replace('<name_category>', category)
+
+# --/-- <id> с базы данных
+def addIdLinks(link, id):
+    return link.replace('<id>', str(id))
+
+
+class TestSmoke(unittest.TestCase):
+
+    def setUp(self):
+        #self.browser = webdriver.Firefox()
+        #self.addCleanup(self.browser.quit)
+
+        self.url = 'http://127.0.0.1:5000'
+        self.title = 'Маркетплейс'
+        self.categories = ['bird', 'eggs', 'fish', 'fruits',
+                           'honey', 'meat', 'milk', 'vegetables'
+                           ]
+        self.links = {'my_orders': '/order_history/<id>', # consumer orders
+                      #'orders': '/producer/<id>/orders/', # producer orders
+                      #'products': '/producer/<id>/products/',
+                      'category': '/category/<name_category>',
+                      'profile': '/user/<id>',
+                      'confirm_order': '/cart/<id>/order_registration/',
+                      }
+        self.id_user = 17
+
+    def testConnection(self):
+        self.assertEqual(200, (urlopen(self.url).getcode()))
+
+        #проверяет корректность категорий по ссылке
+        for category in range(len(self.categories)):
+            for key, value in self.links.items():
+
+                link = addCategoryLinks(value, self.categories[category])
+                link = addIdLinks(link, self.id_user)
+                test_url = self.url + link
+                print(test_url)
+
+                self.assertEqual(200, (urlopen(test_url).getcode()))
+
+    #def testPageTitle(self):
+    #    self.browser.get(self.url)
+    #    self.assertIn(self.title, self.browser.title)
+
+    def testRoutes(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+   unittest.main()
