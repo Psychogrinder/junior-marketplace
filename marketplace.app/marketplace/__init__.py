babd4d18c4b328a71bebd01d5c3b1153ee08a318
Psychogrinder <noirart@gmail.com>
2018-08-20 10:38:42 +0300

    Add styles for order history

--- marketplace.app/marketplace/__init__.py
+++ marketplace.app/marketplace/__init__.py
@@ -21,7 +21,7 @@ db.init_app(app)
 
 from marketplace import models, views, api_rest
 
-css = Bundle('style/base.css','style/header.css', 'style/footer.css', 'style/catalog.css', 'style/modal.css', 'style/category.css', 'style/breadcrumbs.css', 'style/card.css', 'style/cart.css', 'style/edit_profile.css', 'style/profile.css',
+css = Bundle('style/base.css','style/header.css', 'style/footer.css', 'style/catalog.css', 'style/modal.css', 'style/category.css', 'style/breadcrumbs.css', 'style/card.css', 'style/cart.css', 'style/edit_profile.css', 'style/profile.css', 'style/order_history.css',
             filters=['cssmin'], output='bundle.min.css')
 assets.register('css_all', css)
 
