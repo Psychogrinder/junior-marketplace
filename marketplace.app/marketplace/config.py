c9d0ce690506db3b793de8016a1b75b9c64c449f
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-08-22 13:45:12 +0300

    ISSUE-112 в config добвлена инициализация dotenv и поля для отправки email

--- marketplace.app/marketplace/config.py
+++ marketplace.app/marketplace/config.py
@@ -1,4 +1,13 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/marketplace.db'
     SECRET_KEY = 'secret-key'
     SECURITY_PASSWORD_SALT = 'secret-salt'
+
+
+    MAIL_SERVER = 'smtp.mail.ru'
+    MAIL_PORT = 2525
+    MAIL_USE_TLS = True
