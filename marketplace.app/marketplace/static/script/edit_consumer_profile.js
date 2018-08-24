064ce86c2928923de580e215ade6a6559704b22a
Alexey Yunoshev <alexey.yunoshev@gmail.com>
2018-08-24 11:37:11 +0300

    qwe

--- marketplace.app/marketplace/static/script/edit_consumer_profile.js
+++ marketplace.app/marketplace/static/script/edit_consumer_profile.js
@@ -1,14 +1,5 @@
 $(document).ready(function () {
     if ($('main .edit-profile').length > 0) {
-        var consumer;
-        var addr = window.location + '';
-        addr = addr.split('/');
-        var consumer_id = addr[addr.length - 2];
-        $.ajax({
-            url: "/api/v1/users/" + consumer_id,
-            success: function (consumer_data) {
-                console.log(consumer_data);
-            }
-        });
+        console.log("hello")
     }
 });
