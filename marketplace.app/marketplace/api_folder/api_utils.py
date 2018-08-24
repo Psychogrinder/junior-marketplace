681772539062c4ddd27e8f7ec34c02e53df2baaa
Egor Galkin <egor.galkin99@yandex.ru>
2018-08-24 12:59:35 +0300

    ISSUE-132 Added return value to abort_if methods

--- marketplace.app/marketplace/api_folder/api_utils.py
+++ marketplace.app/marketplace/api_folder/api_utils.py
@@ -40,82 +40,89 @@ def no_image_presented():
 # Abort if methods
 
 
-def abort_if_order_doesnt_exist(order_id):
-    if Order.query.get(order_id) is None:
+def abort_if_order_doesnt_exist_or_get(order_id):
+    order = Order.query.get(order_id)
+    if order is None:
         abort(404, message='Order with id = {} doesn\'t exists'.format(order_id))
+    return order
 
 
-def abort_if_consumer_doesnt_exist(consumer_id):
-    if Consumer.query.filter_by(entity='consumer').filter_by(id=consumer_id).first() is None:
+def abort_if_consumer_doesnt_exist_or_get(consumer_id):
+    consumer = Consumer.query.filter_by(entity='consumer').filter_by(id=consumer_id).first()
+    if consumer is None:
         abort(404, message='Consumer with id = {} doesn\'t exists'.format(consumer_id))
+    return consumer
 
 
-def abort_if_producer_doesnt_exist(producer_id):
-    if Producer.query.filter_by(entity='producer').filter_by(id=producer_id).first() is None:
+def abort_if_producer_doesnt_exist_or_get(producer_id):
+    producer = Producer.query.filter_by(entity='producer').filter_by(id=producer_id).first()
+    if producer is None:
         abort(404, message='Producer with id = {} doesn\'t exists'.format(producer_id))
+    return producer
 
 
-def abort_if_category_doesnt_exist(category_id):
-    if Category.query.get(category_id) is None:
+def abort_if_category_doesnt_exist_or_get(category_id):
+    category = Category.query.get(category_id)
+    if category is None:
         abort(404, message='Category with id = {} doesn\'t exists'.format(category_id))
+    return category
 
 
-def abort_if_category_doesnt_exist_slug(category_slug):
-    if Category.query.filter_by(slug=category_slug) is None:
+def abort_if_category_doesnt_exist_slug_or_get(category_slug):
+    category = Category.query.filter_by(slug=category_slug)
+    if category is None:
         abort(404, message='Category with name = {} doesn\'t exists'.format(category_slug))
+    return category
 
 
-def abort_if_product_doesnt_exist(product):
-    if Product.query.get(product) is None:
+def abort_if_product_doesnt_exist_or_get(product):
+    product = Product.query.get(product)
+    if product is None:
         abort(404, message='Product with id = {} doesn\'t exists'.format(product))
+    return product
 
 
 # Get by id methods
 
 def get_orders_by_producer_id(producer_id):
-    abort_if_producer_doesnt_exist(producer_id)
+    abort_if_producer_doesnt_exist_or_get(producer_id)
     return Order.query.filter_by(producer_id=producer_id).all()
 
 
 def get_orders_by_consumer_id(consumer_id):
-    abort_if_consumer_doesnt_exist(consumer_id)
+    abort_if_consumer_doesnt_exist_or_get(consumer_id)
     return Order.query.filter_by(consumer_id=consumer_id).all()
 
 
 def get_order_by_id(order_id):
-    abort_if_order_doesnt_exist(order_id)
-    return Order.query.get(order_id)
+    return abort_if_order_doesnt_exist_or_get(order_id)
 
 
 def get_consumer_by_id(consumer_id):
-    abort_if_consumer_doesnt_exist(consumer_id)
-    return Consumer.query.get(consumer_id)
+    return abort_if_consumer_doesnt_exist_or_get(consumer_id)
 
 
 def get_producer_by_id(producer_id):
-    abort_if_producer_doesnt_exist(producer_id)
-    return Producer.query.get(producer_id)
+    return abort_if_producer_doesnt_exist_or_get(producer_id)
 
 
 def get_category_by_id(category_id):
-    abort_if_category_doesnt_exist(category_id)
-    return Category.query.get(category_id)
+    return abort_if_category_doesnt_exist_or_get(category_id)
 
 
 def get_subcategories_by_category_id(category_id):
-    abort_if_category_doesnt_exist(category_id)
+    abort_if_category_doesnt_exist_or_get(category_id)
     return Category.query.filter_by(parent_id=category_id).all()
 
 
 def get_subcategories_by_category_slug(category_slug):
-    abort_if_category_doesnt_exist_slug(category_slug)
+    abort_if_category_doesnt_exist_slug_or_get(category_slug)
     category_id = Category.query.filter_by(slug=category_slug).first().id
     return Category.query.filter_by(parent_id=category_id).all()
 
 
 def get_product_by_id(product_id):
-    abort_if_product_doesnt_exist(product_id)
-    return Product.query.get(product_id)
+    return abort_if_product_doesnt_exist_or_get(product_id)
 
 
 def get_products_by_category_id(category_id):
@@ -129,10 +136,12 @@ def get_products_by_category_id(category_id):
 
 
 def get_products_by_producer_id(producer_id):
+    abort_if_producer_doesnt_exist_or_get(producer_id)
     return Product.query.filter_by(producer_id=producer_id).all()
 
 
 def get_cart_by_consumer_id(consumer_id):
+    abort_if_consumer_doesnt_exist_or_get()
     cart = Cart.query.filter_by(consumer_id=consumer_id).first()
     return cart if cart is not None else post_cart(consumer_id)
 
@@ -235,8 +244,8 @@ def producer_has_product_with_such_name(args):
 # Post methods
 
 def post_order(args):
-    abort_if_producer_doesnt_exist(args['producer_id'])
-    abort_if_consumer_doesnt_exist(args['consumer_id'])
+    abort_if_producer_doesnt_exist_or_get(args['producer_id'])
+    abort_if_consumer_doesnt_exist_or_get(args['consumer_id'])
     new_order = order_schema.load(args).data
     db.session.add(new_order)
     db.session.commit()
@@ -263,8 +272,8 @@ def post_producer(args):
 
 
 def post_product(args):
-    abort_if_producer_doesnt_exist(args['producer_id'])
-    abort_if_category_doesnt_exist(args['category_id'])
+    abort_if_producer_doesnt_exist_or_get(args['producer_id'])
+    abort_if_category_doesnt_exist_or_get(args['category_id'])
     new_product = product_schema.load(args).data
     db.session.add(new_product)
     producer = get_producer_by_id(args['producer_id'])
@@ -285,7 +294,7 @@ def post_cart(consumer_id):
 
 
 def post_item_to_cart_by_consumer_id(args, consumer_id):
-    abort_if_product_doesnt_exist(int(args['product_id']))
+    abort_if_product_doesnt_exist_or_get(int(args['product_id']))
     cart = get_cart_by_consumer_id(consumer_id)
     cart.put_item(args['product_id'], args['quantity'])
     db.session.commit()
