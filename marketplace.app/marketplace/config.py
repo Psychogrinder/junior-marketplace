2c30eb772dba97525f8a1c505471c3c29bcd0e9a
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-08-24 13:02:30 +0300

    ISSUE-112 добавлены настройки для отпраки почты и брокера для celery

--- marketplace.app/marketplace/config.py
+++ marketplace.app/marketplace/config.py
@@ -1,4 +1,7 @@
 from dotenv import load_dotenv
+import os
+
+
 load_dotenv()
 
 
@@ -7,7 +10,11 @@ class Config(object):
     SECRET_KEY = 'secret-key'
     SECURITY_PASSWORD_SALT = 'secret-salt'
 
-
     MAIL_SERVER = 'smtp.mail.ru'
     MAIL_PORT = 2525
     MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+
+    CELERY_BROKER_URL='redis://localhost:6379/0',
