b02e56145549015555c2931f853a35a5f973caef
Roman Melnychuk <jenikuse@gmail.com>
2018-08-27 18:32:20 +0300

    Добавил тестирование url (users part)

--- marketplace.app/marketplace/tests/smoke_tests.py
+++ marketplace.app/marketplace/tests/smoke_tests.py
@@ -31,20 +31,26 @@ def getCategorySlug():
     return category_slugs
 
 def getUserIds():
-    user_ids = {'producer_ids': [], 'consumer_ids': []}
+    user_ids = {'producer_ids': [], 'user_ids': [], 'count': 0}
 
     for user in User.query.all():
         if user.entity == 'producer':
             user_ids['producer_ids'].append(user.id)
         else:
-            user_ids['consumer_ids'].append(user.id)
+            user_ids['user_ids'].append(user.id)
+        user_ids['count'] += 1
 
-    return user_ids #return dict with users id
+    return user_ids #return dict with user ids
 
 def replaceCategoryName(url, category_slug):
     if '<category_name>' in url:
         return url.replace('<category_name>', category_slug)
 
+def replaceUserId(url, user_id):
+    if '<producer_id>' in url:
+        return url.replace('<producer_id>', str(user_id))
+    elif '<user_id>' in url:
+        return url.replace('<user_id>', str(user_id))
 
 class TestSmoke(unittest.TestCase):
 
@@ -76,7 +82,22 @@ class TestSmoke(unittest.TestCase):
     def testUserRoutes(self):
         user_ids = getUserIds()
 
+        """TODO: Add for all ids"""
+        for route in self.routes:
+            if '<producer_id>' in route:
+                test_url = replaceUserId(self.url + route, user_ids['producer_ids'][0])
+                print(test_url)
+                
+                """TODO: Add product_id"""
+                if '<product_id>' in route:
+                    pass
 
+            elif '<user_id>' in route:
+                test_url = replaceUserId(self.url + route, user_ids['user_ids'][0])
+                print(test_url)
+
+    def testToken(self):
+        pass
 
     def tearDown(self):
         pass
