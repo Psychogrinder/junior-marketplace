0af39b007881f7ee831a6979d8c9e4a247f440d3
Psychogrinder <noirart@gmail.com>
2018-08-29 20:21:16 +0300

    Start implementing validation

--- marketplace.app/marketplace/static/script/create_product.js
+++ marketplace.app/marketplace/static/script/create_product.js
@@ -1,92 +1,116 @@
 $(document).ready(function () {
+    if ($('main .edit-product-card').length > 0) {
 
-    var categoryId;
-    var default_category_id = 1;
-    var addr = window.location + '';
-    addr = addr.split('/');
-    var producerId = addr[addr.length - 2];
+        var categoryId;
+        var default_category_id = 1;
+        var addr = window.location + '';
+        addr = addr.split('/');
+        var producerId = addr[addr.length - 2];
 
-    function createNewProductObject(){
-        categoryId = parseInt($('#createSubcategory option:selected').data('id'));
-        var obj = {
-            name: $('#createName').val(),
-            price: $('#createPrice').val(),
-            producer_id: producerId,
-            category_id: categoryId,
-            quantity: $('#createCount').val(),
-            measurement_unit: $('#createUnits option:selected').val(),
-            weight: $('#createWeigth').val()
-        };
-        return obj;
-    }
+        function createNewProductObject(){
+            categoryId = parseInt($('#createSubcategory option:selected').data('id'));
+            var obj = {
+                name: $('#createName').val(),
+                price: $('#createPrice').val(),
+                producer_id: producerId,
+                category_id: categoryId,
+                quantity: $('#createCount').val(),
+                measurement_unit: $('#createUnits option:selected').val(),
+                weight: $('#createWeigth').val()
+            };
+            return obj;
+        }
 
-    function fillOptions(category_id) {
-        $.ajax({
-            url: "/api/v1/categories/base",
-            success: function (data) {
-                for (var i = 0; i < data.length; i++) {
-                    $("#createCategory").append('<option value="" class="category_option"></option>');
-                }
-
-                var category_option = $('.category_option');
-                for (var i = 0; i < data.length; i++) {
-                    if (data[i].id == category_id) {
-                        var parent_slug = data[i].slug;
+        function fillOptions(category_id) {
+            $.ajax({
+                url: "/api/v1/categories/base",
+                success: function (data) {
+                    for (var i = 0; i < data.length; i++) {
+                        $("#createCategory").append('<option value="" class="category_option"></option>');
                     }
-                    category_option[i].innerHTML = data[i].name;
-                    $(category_option[i]).val(data[i].slug);
-                }
-                getSubcategories(parent_slug)
-            }
-        });
-    }
 
-    function getSubcategories(parent_slug) {
-        $.get("/api/v1/categories/slug/" + parent_slug + "/subcategories/",
-            function (data) {
-                var subcategories = data;
-                for (var i = 0; i < subcategories.length; i++) {
-                    $('#createSubcategory').append('<option value="" class="subcategory_option"></option>')
-                }
-                var subcategory_option = $('.subcategory_option');
-                for (var i = 0; i < subcategories.length; i++) {
-                    subcategory_option[i].innerHTML = subcategories[i].name;
-                    $(subcategory_option[i]).val(subcategories[i].slug);
-                    subcategory_option[i].setAttribute('data-id', subcategories[i].id);
+                    var category_option = $('.category_option');
+                    for (var i = 0; i < data.length; i++) {
+                        if (data[i].id == category_id) {
+                            var parent_slug = data[i].slug;
+                        }
+                        category_option[i].innerHTML = data[i].name;
+                        $(category_option[i]).val(data[i].slug);
+                    }
+                    getSubcategories(parent_slug)
                 }
             });
-    }
+        }
 
-    fillOptions(default_category_id);
+        function getSubcategories(parent_slug) {
+            $.get("/api/v1/categories/slug/" + parent_slug + "/subcategories/",
+                function (data) {
+                    var subcategories = data;
+                    for (var i = 0; i < subcategories.length; i++) {
+                        $('#createSubcategory').append('<option value="" class="subcategory_option"></option>')
+                    }
+                    var subcategory_option = $('.subcategory_option');
+                    for (var i = 0; i < subcategories.length; i++) {
+                        subcategory_option[i].innerHTML = subcategories[i].name;
+                        $(subcategory_option[i]).val(subcategories[i].slug);
+                        subcategory_option[i].setAttribute('data-id', subcategories[i].id);
+                    }
+                });
+        }
 
-    $('#createCategory').on('change', function () {
-        $.get("/api/v1/categories/slug/" + this.value + "/subcategories/",
-            function (data) {
-                var subcategories = data;
-                $('.subcategory_option').remove();
-                for (var i = 0; i < subcategories.length; i++) {
-                    $('#createSubcategory').append('<option value="" class="subcategory_option"></option>')
-                }
-                var subcategory_option = $('.subcategory_option');
-                for (var i = 0; i < subcategories.length; i++) {
-                    subcategory_option[i].innerHTML = subcategories[i].name;
-                    $(subcategory_option[i]).val(subcategories[i].slug);
-                    subcategory_option[i].setAttribute('data-id', subcategories[i].id);
-                }
-            });
-    });
+        fillOptions(default_category_id);
 
-    $('#createProductSave').click(function(){
-        var newProductObject = createNewProductObject();
+        $('#createCategory').on('change', function () {
+            $.get("/api/v1/categories/slug/" + this.value + "/subcategories/",
+                function (data) {
+                    var subcategories = data;
+                    $('.subcategory_option').remove();
+                    for (var i = 0; i < subcategories.length; i++) {
+                        $('#createSubcategory').append('<option value="" class="subcategory_option"></option>')
+                    }
+                    var subcategory_option = $('.subcategory_option');
+                    for (var i = 0; i < subcategories.length; i++) {
+                        subcategory_option[i].innerHTML = subcategories[i].name;
+                        $(subcategory_option[i]).val(subcategories[i].slug);
+                        subcategory_option[i].setAttribute('data-id', subcategories[i].id);
+                    }
+                });
+        });
 
-        $.ajax({
-            url: '/api/v1/products',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(newProductObject),
-            success: function(data, status) {
+        $('#createName').on('change', function () {
+            console.log('createName changed');
+            // var productName = $('#createName').val();
+            // console.log(productName);
+            // if (productName.length < 1) {
+            //     $('#createName').css("border-color", "#FF7851");
+            // } else {
+            //     $('#createName').css("border-color", "#ced4da");
+            // }
+        });
 
+        $('#createPrice').on('change', function () {
+            console.log('hell price');
+            var productName = $('#createPrice').val();
+            console.log(productName);
+            if (productName.length < 1) {
+                $('#createPrice').css("border-color", "#FF7851");
+            } else {
+                $('#createPrice').css("border-color", "#ced4da");
             }
         });
-    });
+
+        $('#createProductSave').click(function(){
+            var newProductObject = createNewProductObject();
+
+            $.ajax({
+                url: '/api/v1/products',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(newProductObject),
+                success: function(data, status) {
+
+                }
+            });
+        });
+    }
 });
