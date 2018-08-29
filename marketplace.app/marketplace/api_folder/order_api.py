767f7a28e9abcce00c059159234e9dbeb810ffe8
Alexey Yunoshev <alexey.yunoshev@gmail.com>
2018-08-29 11:55:33 +0300

    ISSUE-150

--- marketplace.app/marketplace/api_folder/order_api.py
+++ marketplace.app/marketplace/api_folder/order_api.py
@@ -28,3 +28,7 @@ class Orders(Resource):
     def put(self, order_id):
         args = parser.parse_args()
         return order_schema.dump(utils.put_order(args, order_id)).data, 201
+
+    def delete(self, order_id):
+        utils.delete_order_by_id(order_id)
+        return "Заказ был успешно удалён", 202
