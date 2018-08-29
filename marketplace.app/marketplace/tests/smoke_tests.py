1b846cf1e4079325cf7d2ccc84ad38e42dca5cf6
Roman Melnychuk <jenikuse@gmail.com>
2018-08-29 14:46:34 +0300

    Исправил проверку респонса роутов, сделал рефакторинг

--- marketplace.app/marketplace/tests/smoke_tests.py
+++ marketplace.app/marketplace/tests/smoke_tests.py
@@ -3,12 +3,12 @@ from path_file import *
 from marketplace.models import Category, User, Product, Producer
 import unittest
 from urllib.request import Request, urlopen
+
+from urllib.error import HTTPError
 from flask_login import current_user, login_user, logout_user, login_required
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-unittest.TestLoader.sortTestMethodsUsing = None
 
 
 def parseApiRoutes():
@@ -16,8 +16,7 @@ def parseApiRoutes():
     routes = {'auth': [],
               'not_auth': ['/category/<category_name>',
                            '/products/<product_id>',
-                           '/producer/<producer_id>',
-                           '/producer/<producer_id>/products'
+                           '/producer/<producer_id>'
                            ]
               }
 
@@ -29,28 +28,26 @@ def parseApiRoutes():
                 first_symbol, last_symblol = s.find('/'), s.rfind('\'')
                 route = s[first_symbol:last_symblol]
 
-                #роуты, доступные (не-)авторизованным пользователям
                 if route not in (routes['not_auth'] and routes['not_auth']):
-                    if ('<' or '>') not in route:
+                    if ('<' or '>' or 'products') not in route:
                         routes['not_auth'].append(route)
                     else:
                         routes['auth'].append(route)
-
-    print(routes['not_auth'])
-    print(routes['auth'])
+    return routes
 
 
 def getCategorySlugs():
     category_slugs = []
-
     for category in Category.query.all():
         category_slugs.append(category.slug)
 
     return category_slugs
 
+
 def replaceCategoryName(url, category_slug):
     if '<category_name>' in url:
         return url.replace('<category_name>', category_slug)
+
 
 def getUserIds():
     user_ids = {'producer_ids': [], 'user_ids': [], 'count': 0}
@@ -64,11 +61,13 @@ def getUserIds():
 
     return user_ids #return dict with user ids
 
+
 def replaceUserId(url, user_id):
     if '<producer_id>' in url:
         return url.replace('<producer_id>', str(user_id))
     elif '<user_id>' in url:
         return url.replace('<user_id>', str(user_id))
+
 
 def getProductIds():
     product_ids = []
@@ -78,9 +77,21 @@ def getProductIds():
 
     return product_ids
 
+
 def replaceProductId(url, product_id):
     if '<product_id>' in url:
         return url.replace('<product_id>', str(product_id))
+
+
+def getResponseCode(url):
+    request = Request(url=url)
+    try:
+        response = urlopen(request).getcode()
+    except HTTPError as e:
+        response = e.read()
+
+    return response
+
 
 class TestSmoke(unittest.TestCase):
 
@@ -88,6 +99,8 @@ class TestSmoke(unittest.TestCase):
         self.url = 'http://127.0.0.1:8000'
         self.routes = parseApiRoutes()
         self.category_slugs = getCategorySlugs()
+        self.user_ids = getUserIds()
+        self.product_ids = getProductIds()
 
     def testConnection(self):
         self.assertEqual(200, (urlopen(self.url).getcode()))
@@ -95,41 +108,46 @@ class TestSmoke(unittest.TestCase):
         print('Connection is OK\n')
 
     def testBaseRoutes(self):
-        for route in self.routes[1:]:
+        for route in self.routes['not_auth']:
             test_url = self.url + route
-            if '<' not in test_url:
-                self.assertEqual(200, (urlopen(test_url).getcode()))
 
-                print(test_url)
-        print('Base routes are OK.\n')
+            if '<category_name>' in route:
+                for category_slug in self.category_slugs:
+                    test_url = replaceCategoryName(self.url + route, category_slug)
 
-    def testCategoryRoutes(self):
-        matching = [route for route in self.routes if '<category_name>' in route]
+            elif '<producer_id>' in route:
+                test_url = replaceUserId(test_url, self.user_ids['producer_ids'][0])
 
-        for category_slug in self.category_slugs:
-            test_url = replaceCategoryName(self.url + matching[0], category_slug)
+            elif '<product_id>' in route:
+                test_url = replaceProductId(test_url, self.product_ids[0])
+
+            print(test_url)
             self.assertEqual(200, (urlopen(test_url).getcode()))
 
-    def testUserRoutes(self):
-        user_ids = getUserIds()
-        product_ids = getProductIds()
+        print('Base routes are OK.\n')
 
-        """TODO: Add for all ids"""
-        for route in self.routes:
+
+    def testRoutesAccessRights(self):
+
+        """TODO: Add for all ids and Check token links"""
+        for route in self.routes['auth']:
+
             test_url = self.url + route
 
             if '<producer_id>' in route:
-                test_url = replaceUserId(test_url, user_ids['producer_ids'][0])
-                if '<product_id>' not in route:
-                    print(test_url)
+                test_url = replaceUserId(test_url, self.user_ids['producer_ids'][0])
 
             elif '<user_id>' in route:
-                test_url = replaceUserId(test_url, user_ids['user_ids'][0])
-                print(test_url)
+                test_url = replaceUserId(test_url, self.user_ids['user_ids'][0])
 
             if '<product_id>' in route:
-                test_url = replaceProductId(test_url, product_ids[0])
-                print(test_url)
+                test_url = replaceProductId(test_url, self.product_ids[0])
+
+            print(test_url)
+            self.assertNotEqual(200, getResponseCode(test_url))
+
+        print('Auth routes are OK.\n')
+
 
     def testToken(self):
         pass
@@ -138,4 +156,7 @@ class TestSmoke(unittest.TestCase):
         pass
 
 if __name__ == '__main__':
-   unittest.main()
+   unittest.TestLoader.sortTestMethodsUsing = None
+
+   suite = unittest.TestLoader().loadTestsFromTestCase(TestSmoke)
+   unittest.TextTestRunner(verbosity=2).run(suite)
