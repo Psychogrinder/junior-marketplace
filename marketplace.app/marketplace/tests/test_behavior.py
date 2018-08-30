d13c67b5f3b2119cc074cc0941815f3fa2ba05d8
Roman Melnychuk <jenikuse@gmail.com>
2018-08-30 16:16:59 +0300

    fix api_routes

--- marketplace.app/marketplace/tests/test_behavior.py
+++ marketplace.app/marketplace/tests/test_behavior.py
@@ -1,12 +1,14 @@
 from path_file import *
 
 import unittest
-from mock import Mock
+from mock import Mock, patch
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Firefox
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestCase(unittest.TestCase):
@@ -30,21 +32,34 @@ class TestCase(unittest.TestCase):
 
         user = self.user
         browser = self.browser
-        browser.maximize_window()
+
         browser.get('http://127.0.0.1:8000')
 
         # signUpUser
         #browser.find_element_by_id('emailRegistration')#.send_keys('abba@mail.ru')
+        browser.find_element_by_css_selector('form-control mr-sm-2')#.send_keys('abba@mail.ru')
+        #browser.find_element_by_class_name('form-control mr-sm-2').send_keys(Keys.RETURN)
+        #browser.close()
 
-        browser.find_element_by_id('emailAuthorisation').send_keys(user.email)
-        browser.find_element_by_id('passwordAuthorisation').send_keys(user.password)
-        browser.find_element_by_id('authButton').click()
+
+        '''try:
+            element = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.ID, "emailAuthorisation"))
+            )
+        finally:
+            browser.quit()
+        print(element)
+        '''
+
+        #browser.find_element_by_id('emailAuthorisation').send_keys(user.email)
+        #browser.find_element_by_id('passwordAuthorisation').send_keys(user.password)
+        #browser.find_element_by_id('authButton').click()
 
 
 
     def tearDown(self):
-        self.browser.close()
-
+     #   self.browser.close()
+        self.browser.quit()
 
 if __name__ == '__main__':
     unittest.main()
