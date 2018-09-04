54090f55bfbad43b1ed3a1fd83a4f6d124fbf2d2
Kalinin Kirill <34929836+kalinink@users.noreply.github.com>
2018-09-04 16:30:34 +0300

    ISSUE-196 удалил DUMP_DIR=$(docker inspect marketplacedb --format='{{.Mountpoint}}')

--- marketplace.db/db_dump.sh
+++ marketplace.db/db_dump.sh
@@ -4,7 +4,7 @@ set -x
 
 source ../DOCKER_ENV_PRODUCTION
 
-DUMP_DIR=$(docker inspect marketplacedb --format='{{.Mountpoint}}')
+
 
 docker exec marketplace.db \
     pg_dump -U postgres marketplace.db | \
