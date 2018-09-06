b893b44e397721534095ab8463178952a9febcfe
Alexey Yunoshev <alexey.yunoshev@gmail.com>
2018-09-06 13:08:55 +0300

    refactor

--- marketplace.app/marketplace/api_routes.py
+++ marketplace.app/marketplace/api_routes.py
@@ -36,33 +36,50 @@ from marketplace.api_folder.product_api import (
     ProductSearchByParams,
 )
 
-# TODO CHECK
-
+# Orders
 api.add_resource(GlobalOrders, '/orders')
 api.add_resource(Orders, '/orders/<int:order_id>')
 api.add_resource(ConsumerOrders, '/consumers/<int:consumer_id>/orders')
 api.add_resource(ProducerOrders, '/producers/<int:producer_id>/orders')
+api.add_resource(UnprocessedOrdersByProducerId, '/producers/<int:producer_id>/unprocessed_orders')
+api.add_resource(FilteredOrdersByProducerId, '/producers/filtered_orders')
+
+# Cart
 api.add_resource(GlobalCart, '/consumers/<int:consumer_id>/cart')
 api.add_resource(NumberOfProductsInCart, '/consumers/<int:consumer_id>/cart/quantity')
+api.add_resource(ProductsInCart, '/products/<int:consumer_id>/cart')
+
+# Authorization
 api.add_resource(Login, '/login')
 api.add_resource(Logout, '/logout')
+
+# Upload
 api.add_resource(UploadImageConsumer, '/consumers/<int:consumer_id>/upload')
 api.add_resource(UploadImageProducer, '/producers/<int:producer_id>/upload')
 api.add_resource(UploadImageProduct, '/products/<int:product_id>/upload')
+
+# Category
 api.add_resource(CategoryRest, '/categories/<string:slug>')
-api.add_resource(ProductsInCart, '/products/<int:consumer_id>/cart')
+api.add_resource(BaseCategories, '/categories/base')
+api.add_resource(ParentCategoryBySubcategoryId, '/categories/<int:category_id>/parent')
 api.add_resource(SubcategoryNamesByProducerName, '/categories/producer/<string:producer_name>')
 api.add_resource(SubcategoryNamesByParentSlugAndProducerName,
                  '/categories/<string:parent_category_slug>/producer/<string:producer_name>')
 api.add_resource(ProducerNamesByCategoryName, '/producers/<string:category_name>')
-api.add_resource(UnprocessedOrdersByProducerId, '/producers/<int:producer_id>/unprocessed_orders')
-api.add_resource(ProducerNameById, '/producers/<int:producer_id>/name')
-api.add_resource(FilteredOrdersByProducerId, '/producers/filtered_orders')
-# checked
-api.add_resource(BaseCategories, '/categories/base')
 api.add_resource(Subcategories, '/categories/<int:category_id>/subcategories/')
-api.add_resource(ParentCategoryBySubcategoryId, '/categories/<int:category_id>/parent')
 api.add_resource(SubcategoriesBySlug, '/categories/slug/<string:category_slug>/subcategories/')
+
+
+# Producers
+api.add_resource(GlobalProducers, '/producers')
+api.add_resource(ProducerRest, '/producers/<int:producer_id>')
+api.add_resource(ProducerNameById, '/producers/<int:producer_id>/name')
+
+# Consumers
+api.add_resource(GlobalConsumers, '/consumers')
+api.add_resource(ConsumerRest, '/consumers/<int:consumer_id>')
+
+# Products
 api.add_resource(GlobalProducts, '/products')
 api.add_resource(ProductRest, '/products/<int:product_id>')
 api.add_resource(ProductSearchByParams, '/products/search')
@@ -70,9 +87,5 @@ api.add_resource(ProductsByCategory, '/categories/<int:category_id>')
 api.add_resource(PopularProductsByCategory, '/categories/<int:category_id>/popularity/<string:direction>')
 api.add_resource(ProductsByPrice, '/categories/<int:category_id>/price/<string:direction>')
 api.add_resource(ProductsByProducer, '/producers/<int:producer_id>/products')
-api.add_resource(GlobalConsumers, '/consumers')
-api.add_resource(ConsumerRest, '/consumers/<int:consumer_id>')
-api.add_resource(GlobalProducers, '/producers')
-api.add_resource(ProducerRest, '/producers/<int:producer_id>')
 api.add_resource(PopularProducts, '/products/popular')
 api.add_resource(ProductsSortedAndFiltered, '/products/filter')
