f55151d8d83c4bf9b82931e0482a0bb46f897ec6
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-09-06 17:17:36 +0300

    ISSUE-203 добавил функционал для обработки изображений celery

--- marketplace.app/marketplace/api_folder/utils/uploaders.py
+++ marketplace.app/marketplace/api_folder/utils/uploaders.py
@@ -2,19 +2,36 @@ import os
 import time
 
 from werkzeug.utils import secure_filename
-
-from marketplace import db, app
-from marketplace.api_folder.utils.abortions import no_file_part_in_request, no_image_presented
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+from marketplace import models
+from marketplace import image_tools
+from marketplace import db, app, celery
+from marketplace.api_folder.utils.abortions import (
+    no_file_part_in_request,
+    no_image_presented
+)
 
 
 def allowed_extension(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    extension = os.path.splitext(filename)[1].lower().replace('.', '')
+    return extension in app.config['ALLOWED_UPLOAD_EXTENSIONS']
 
 
-def upload_image(uploader, files, producer_id, product_id=None):
+@celery.task()
+def commit_change(image_url, cls, uploader_id):
+    model = getattr(models, cls)
+    instance = model.query.get(uploader_id)
+    instance.set_photo_url(image_url)
+    db.session.commit()
+
+
+def save_upload_image(image_url, uploader, size):
+    image_tools.change_image.apply_async(
+        (image_url, size),
+        link=commit_change.s(type(uploader).__name__, uploader.id)
+    )
+
+
+def upload_image(uploader, files, producer_id, size, product_id=None):
     if files:
         if 'image' not in files:
             no_file_part_in_request()
@@ -22,12 +39,13 @@ def upload_image(uploader, files, producer_id, product_id=None):
         if image.filename == '':
             no_image_presented()
         if image and allowed_extension(image.filename):
-            image_url = os.path.join(app.config['UPLOAD_FOLDER'], str(producer_id),
-                                     f'{hash(str(time.time()))}_' + secure_filename(image.filename))
+            image_url = os.path.join(
+                app.config['UPLOAD_FOLDER'],
+                str(producer_id),
+                f'{hash(time.time())}_' + secure_filename(image.filename)
+            )
             image.save(image_url)
-            image_url = '/'.join(image_url.split('/')[-5:])
-            uploader.set_photo_url(image_url)
-            db.session.commit()
-        return '/' + image_url
+            save_upload_image(image_url, uploader, size)
+        return '/'+image_tools.get_static_name(image_url)
     else:
         return False
