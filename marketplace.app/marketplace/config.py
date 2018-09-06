59a42d3af534f898c3dd1f1faf1e295a87b40126
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-09-06 17:16:23 +0300

    ISSUE-203 добавил CELERY_BACKEND_URL, USER_IMAGE_PRODUCTS_SIZE, USER_IMAGE_PRODUCER_LOGO_SIZE, ALLOWED_UPLOAD_EXTENSIONS, USER_IMAGE_DEFAULT_FORMAT

--- marketplace.app/marketplace/config.py
+++ marketplace.app/marketplace/config.py
@@ -12,6 +12,11 @@ class Base(object):
         'static', 'img', 'user_images'
     )
 
+    USER_IMAGE_PRODUCTS_SIZE = 255, 150
+    USER_IMAGE_PRODUCER_LOGO_SIZE = 250, 300
+    ALLOWED_UPLOAD_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'webp'])
+    USER_IMAGE_DEFAULT_FORMAT = 'webp'
+
     MAIL_SERVER = 'smtp.mail.ru'
     MAIL_PORT = 2525
     MAIL_USE_TLS = True
@@ -21,7 +26,8 @@ class Base(object):
     MAIL_DEFAULT_SENDER = 'xtramarket@rambler.ru'
 
 
-    CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL') or 'redis://localhost:6379/0',
+    CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL') or 'redis://localhost:6379/0'
+    CELERY_BACKEND_URL = os.getenv('CELERY_BACKEND_URL') or 'redis://localhost:6379/9'
 
     CACHE_STORAGE_HOST = 'localhost'
     CACHE_STORAGE_PORT = 6379
@@ -38,8 +44,6 @@ class Development(Base):
     SECRET_KEY = 'secret-key'
     SECURITY_PASSWORD_SALT = 'secret-salt'
 
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
-
     CACHE_STORAGE_HOST = os.getenv('CACHE_STORAGE_HOST') or 'localhost'
 
 
@@ -49,6 +53,7 @@ class Production(Base):
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
     CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+    CELERY_BACKEND_URL = os.getenv('CELERY_BACKEND_URL')
 
     SECRET_KEY = os.getenv('SECRET_KEY')
     SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT')
