6b8454d397755fac74996049aa0338f20f0e43e9
Kirill Kalinin <kalinin-kirill@mail.ru>
2018-09-08 18:25:34 +0300

    ISSUE-203 добавил создание volume marketplace_tasks

--- marketplace.app/deploy-prod.sh
+++ marketplace.app/deploy-prod.sh
@@ -4,6 +4,7 @@ source ../DOCKER_ENV_PRODUCTION
 source ../.env.prod
 
 docker volume create marketplace_user_image
+docker volume create marketplace_tasks
 docker network create marketplace
 
 docker-compose -f docker-compose-prod.yml up --build
