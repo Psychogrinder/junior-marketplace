e4ab5ae0b109583ece66dc87d05e7f23fe71e9e1
Roman Melnychuk <jenikuse@gmail.com>
2018-09-10 15:35:35 +0300

    Добавил тесты для фильтрации по производителю

--- marketplace.app/marketplace/tests/testFiltering.py
+++ marketplace.app/marketplace/tests/testFiltering.py
@@ -4,6 +4,8 @@ from testMethods import getCategorySlugs, getResponse, getResponseCode, parseApi
 
 from marketplace.api_folder.utils.product_utils import get_sorted_and_filtered_products
 
+from marketplace.api_folder.utils.producer_utils import get_all_producers
+
 import unittest
 
 
@@ -39,30 +41,49 @@ class TestCase(unittest.TestCase):
                      'price': None,         #down, up
                      'category_name': None, #slugs
                      'producer_name': None,
-                     'in_stock': None,      #1 
+                     'in_stock': None,      #1
         }
-
-        self.slugs = getCategorySlugs()
 
 
     def test_filtered_price(self):
-
         args = self.args
+        self.slugs = getCategorySlugs()
 
         for slug in self.slugs:
             args['category_name'] = slug
 
             args['price'] = 'down'
             sorted = get_sorted_and_filtered_products(args)
-            self.assertEqual(True, check_price(sorted, args['price']))
-
+            self.assertTrue(check_price(sorted, args['price']), 'Problem with DOWN sorting price')
 
             args['price'] = 'up'
             sorted = get_sorted_and_filtered_products(args)
-            self.assertEqual(True, check_price(sorted, args['price']))
+            self.assertTrue(check_price(sorted, args['price']), 'Problem with UP sorting price')
 
+
+    def test_filtered_by_producer(self):
+        args = self.args
+
+        producers = get_all_producers()
+
+        for producer in producers:
+
+            args['producer_name'] = producer.name
+            sorted = get_sorted_and_filtered_products(args)
+
+            for prod in range(len(sorted)):
+                self.assertEqual(producer.name, sorted[prod]['producer_name'])
+
+
+    def test_filtered_popularity(self):
+        args = self.args
+
+
+    def suite(self):
+        return suite
 
 
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
+    unittest.TextTestRunner(verbosity=2).run(suite)
