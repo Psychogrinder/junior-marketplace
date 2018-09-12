9c868d200ff1db537ff04ce0c86dc73254cef5ef
Roman Melnychuk <jenikuse@gmail.com>
2018-09-12 19:01:29 +0300

    ISSUE-246 added more asserts for testing posted comment

--- marketplace.app/marketplace/tests/test_comment_utils.py
+++ marketplace.app/marketplace/tests/test_comment_utils.py
@@ -5,7 +5,8 @@ from testing_utils import parseApiRoutes, replaceUserId, replaceProductId, getRe
 from marketplace.api_folder.utils.comment_utils import get_comment_by_id, get_comments_by_product_id, \
     get_comments_by_consumer_id, post_comment, delete_comment_by_id
 
-from marketplace.api_folder.utils.order_utils import get_orders_by_consumer_id, get_filtered_orders
+from marketplace.api_folder.utils.order_utils import get_orders_by_consumer_id, get_filtered_orders, put_order
+
 import requests
 import json
 
@@ -19,7 +20,7 @@ class TestCase(unittest.TestCase):
         self.ids = [1, 2, 3, 5, 0, 10, 15, 40, 60, -1, 100, 10000, -11.0]
 
     @unittest.skip # FIXME add more args
-    def test_01_get_comment(self):
+    def test_01_get_comments(self):
 
         "TODO: for all products"
         product_id = 1
@@ -41,27 +42,41 @@ class TestCase(unittest.TestCase):
 
     def test_02_post_comment(self):
         login_url = 'http://127.0.0.1:8000/api/v1/login?email=42mail.ru&password=123123'
+        url = 'http://127.0.0.1:8000/api/v1/comments'
+        body_msg = 'Raise of NASA #'
+
         s = requests.Session()
         response = s.post(login_url)
-        user_id = json.loads(response.content)['id']
-        orders = get_orders_by_consumer_id(user_id)
 
-        product_id, order_id = False, False
+        consumer_id = json.loads(response.content)['id']
+        orders = get_orders_by_consumer_id(consumer_id)
+
+        product_id, order_id, consumer_name = False, False, False
         for order in orders:
             if order.status == 'Завершён':
                 for key in order.order_items_json:
                     product_id = key
                     order_id = order.id
+                    consumer_name = order.first_name + ' ' + order.last_name
+                    body_msg += str(order.id)
 
-        if product_id and order_id:
-            url = 'http://127.0.0.1:8000/api/v1/comments'
-            response = s.post(url, data={'consumer_id': user_id,
+        if product_id and order_id and consumer_name:
+            response = s.post(url, data={'consumer_id': consumer_id,
                                          'product_id': product_id,
                                          'order_id': order_id,
-                                         'body': 'Raise of NASA'}
+                                         'consumer_name': consumer_name,
+                                         'body': body_msg,
+                                         }
             )
-            data = json.loads(response.content)
-            print(data)
+            comment = json.loads(response.content)
+
+            self.assertEqual(201, response.status_code)
+            self.assertEqual(consumer_name, comment['consumer_name'])
+            self.assertEqual(body_msg, comment['body'])
+
+            # [OPTIMIZE] product_id is integer, consumer_id is string. What's wrong with u?
+            self.assertEqual(product_id, str(comment['product_id']))
+            self.assertEqual(consumer_id, comment['consumer_id'])
 
 
     def test_03_get_comment_by_id(self):
