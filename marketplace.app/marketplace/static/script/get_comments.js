9b915a4455d069be18456b8a4c0d783cd5aa5b82
IrinaZorkova <irinanazorkova@gmail.com>
2018-09-12 16:06:47 +0300

    temp commit

--- marketplace.app/marketplace/static/script/get_comments.js
+++ marketplace.app/marketplace/static/script/get_comments.js
@@ -36,17 +36,12 @@ function getComments(page_number) {
             } else {
                 $("#commentsNextPage").hide();
             }
-
-
         },
-        error: function (xhr) {
-            console.log(xhr)
-        }
     });
 }
 
 function updateCommentSection(page_number) {
-    getComments(page_number)
+    getComments(page_number);
 }
 
 $("#commentsNextPage").click(function () {
