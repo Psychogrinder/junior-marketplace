de2d86ae45ec502240012c8d9b92faaf2d9a4977
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-09-13 12:17:08 +0300

    ISSUE-25 добавил удалени базы при пересоздании ее из дампа

--- marketplace.db/db_restore.sh
+++ marketplace.db/db_restore.sh
@@ -6,6 +6,7 @@ source ../DOCKER_ENV_PRODUCTION
 
 if [[ $2 == "create" ]]
 then
+  docker exec marketplace.db psql -c 'drop database "marketplace.db";' -U postgres && \
   docker exec marketplace.db psql -c 'create database "marketplace.db";' -U postgres
 fi
 
