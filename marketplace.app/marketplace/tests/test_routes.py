c8ed5750070abcc80ed29c2e9fed0240b6180535
Roman Melnychuk <jenikuse@gmail.com>
2018-09-14 13:23:42 +0300

    Added testing logout request

--- marketplace.app/marketplace/tests/test_routes.py
+++ marketplace.app/marketplace/tests/test_routes.py
@@ -5,6 +5,7 @@ from testing_utils import parseRoutes, get_route_by_name, getCategorySlugs,  get
 import unittest
 from urllib.request import urlopen
 import requests
+import json
 
 class TestSmoke(unittest.TestCase):
 
@@ -24,7 +25,7 @@ class TestSmoke(unittest.TestCase):
     def test_02_login(self):
         routes = self.routes['Authorization']
         url = self.url + get_route_by_name(routes, 'login')
-        print(url)
+
         login_data = {'emails': ['15mail.ru', '50mail.ru', 'melissa.clark@example.com'],
                       'password': 123123,
                       }
@@ -32,36 +33,46 @@ class TestSmoke(unittest.TestCase):
         for email in login_data['emails']:
             response = requests.post(url, data={'email': email, 'password': login_data['password']})
             self.assertEqual(201, response.status_code)
-    
+        print('login is OK')
 
-    # def test_02_logout(self):
-    #     logout_url = self.base_url + '/api/v1/logout'
-    #     response = requests.Session().get(logout_url)
-    #
-    #     self.assertEqual(201, response.status_code)
-    #     self.assertNotIn('session', response.cookies)
+    def test_03_logout(self):
+        routes = self.routes['Authorization']
+        url = self.url + get_route_by_name(routes, 'logout')
 
+        response = requests.get(url)
+        content = json.loads(response.content)
 
-    @unittest.skip
-    def test_02_basic_routes(self):
-
-        for route in self.routes:
-            test_url = self.url + route
+        self.assertEqual(201, response.status_code)
+        self.assertIn('logout', content.lower())
+        print('logout is OK')
 
 
-            if '<category_name>' in route:
-                for category_slug in self.category_slugs:
-                    test_url = replaceCategoryName(self.url + route, category_slug)
+    def test_04_basic_routes(self):
 
-            elif '<producer_id>' in route:
-                test_url = replaceUserId(test_url, self.user_ids['producer_ids'][0])
+        routes = self.routes
+        print(routes.keys())
+        url = self.url + get_route_by_name(routes, 'logout')
 
-            elif '<product_id>' in route:
-                test_url = replaceProductId(test_url, self.product_ids[0])
 
-            print(test_url)
-            self.assertEqual(200, (urlopen(test_url).getcode()))
-        print('Base routes are OK.\n')
+
+
+        # for route in self.routes:
+        #     test_url = self.url + route
+        #
+        #
+        #     if '<category_name>' in route:
+        #         for category_slug in self.category_slugs:
+        #             test_url = replaceCategoryName(self.url + route, category_slug)
+        #
+        #     elif '<producer_id>' in route:
+        #         test_url = replaceUserId(test_url, self.user_ids['producer_ids'][0])
+        #
+        #     elif '<product_id>' in route:
+        #         test_url = replaceProductId(test_url, self.product_ids[0])
+        #
+        #     print(test_url)
+        #     self.assertEqual(200, (urlopen(test_url).getcode()))
+        # print('Base routes are OK.\n')
 
 
 
