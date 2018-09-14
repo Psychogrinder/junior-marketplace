ad5323b7660f521751d24b50ce82a2b2851f5cde
Psychogrinder <noirart@gmail.com>
2018-09-14 16:39:01 +0300

    ISSUE-281 Add validation for quantity field on edit product page

--- marketplace.app/marketplace/static/script/edit_product_post.js
+++ marketplace.app/marketplace/static/script/edit_product_post.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     var categoryId = null;
 
-
     function uploadProductImage(product_id) {
         var image_data = $('#item-img-output').attr('src');
         image_data = image_data.split(',')[1];
@@ -17,39 +16,47 @@ $(document).ready(function () {
         });
     }
 
-    $('#save_product_data').click(function (event) {
-        event.preventDefault();
-        var addr = window.location + '';
-        addr = addr.split('/');
-        var product_id = addr[addr.length - 2];
-        var categorySlug = $('#editSubcategory option:selected').val();
+    $('#save_product_data').click(function () {
+        function submitEditProductForm(){
+            var addr = window.location + '';
+            addr = addr.split('/');
+            var product_id = addr[addr.length - 2];
+            var categorySlug = $('#editSubcategory option:selected').val();
+            categoryId = parseInt($('#editSubcategory option:selected').data('id'));
 
-        categoryId = parseInt($('#editSubcategory option:selected').data('id'));
+            function createProductObject(categoryId) {
+                var productObject = {
+                    name: $('#editName').val(),
+                    price: $('#editPrice').val(),
+                    category_id: $('#editSubcategory').val(),
+                    quantity: $('#editCount').val(),
+                    weight: $('#editWeight').val(),
+                    measurement_unit: $('#measurmentSelect option:selected').val(),
+                    description: $('#editDescription').val()
+                };
+                return productObject;
+            }
 
-        function createProductObject(categoryId) {
-            var productObject = {
-                name: $('#editName').val(),
-                price: $('#editPrice').val(),
-                category_id: $('#editSubcategory').val(),
-                quantity: $('#editCount').val(),
-                weight: $('#editWeight').val(),
-                measurement_unit: $('#measurmentSelect option:selected').val(),
-                description: $('#editDescription').val()
-            };
-            return productObject;
+
+            $.ajax({
+                url: '/api/v1/products/' + product_id,
+                type: 'PUT',
+                contentType: 'application/json',
+                data: JSON.stringify(createProductObject(categoryId)),
+                success: function (data, status) {
+                    uploadProductImage(product_id);
+                    location.replace('/products/' + product_id);
+                }
+            });
+            var hulla = new hullabaloo();
+            hulla.send("Информация о товаре сохранена", "secondary");
         }
 
-        $.ajax({
-            url: '/api/v1/products/' + product_id,
-            type: 'PUT',
-            contentType: 'application/json',
-            data: JSON.stringify(createProductObject(categoryId)),
-            success: function (data, status) {
-                uploadProductImage(product_id);
-                location.replace('/products/' + product_id);
-            }
+
+
+        $('#editProductForm').submit(function (event) {
+            event.preventDefault();
+            submitEditProductForm();
         });
-        var hulla = new hullabaloo();
-        hulla.send("Информация о товаре сохранена", "secondary");
     });
 });
