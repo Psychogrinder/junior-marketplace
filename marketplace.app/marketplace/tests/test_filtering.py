68ed0de3487f7cf5d5ea2d025ec3ce4cc4068bb3
Roman Melnychuk <jenikuse@gmail.com>
2018-09-17 11:42:24 +0300

    ISSUE-246 ordered test cases

--- marketplace.app/marketplace/tests/test_filtering.py
+++ marketplace.app/marketplace/tests/test_filtering.py
@@ -1,19 +1,22 @@
 from path_file import *
 
 
-from testing_utils import getCategorySlugs, is_price_sorted, check_price
+from testing_utils import getCategorySlugs, is_price_sorted, check_price, parseRoutes, \
+    get_route_by_name
 
 from marketplace.api_folder.utils.product_utils import get_sorted_and_filtered_products
 from marketplace.api_folder.utils.producer_utils import get_all_producers
 
 import unittest
+import requests
 
 
 class TestCase(unittest.TestCase):
     unittest.TestLoader.sortTestMethodsUsing = None
 
     def setUp(self):
-
+        self.url = 'http://127.0.0.1:8000/api/v1'
+        self.routes = parseRoutes()
         self.args = {'popularity': None,    # down
                      'price': None,         # down, up
                      'category_name': None, # slugs
@@ -26,7 +29,7 @@ class TestCase(unittest.TestCase):
                      }
 
 
-    def test_filtered_price(self):
+    def test_01_filtered_price(self):
         args = self.args
         args['parent_id'] = 0
 
@@ -44,7 +47,7 @@ class TestCase(unittest.TestCase):
             self.assertTrue(check_price(sorted, args['price']), 'Problem with UP sorting price')
 
 
-    def test_filtered_by_producer(self):
+    def test_02_filtered_by_producer(self):
         args = self.args
         producers = get_all_producers()
 
@@ -57,8 +60,14 @@ class TestCase(unittest.TestCase):
                 self.assertEqual(producer.name, sorted['products'][product]['producer_name'])
 
 
-    def test_filtered_popularity(self):
-        args = self.args
+    def test_03_search(self):
+        routes = self.routes['Products']
+        url = self.url + get_route_by_name(routes, 'search') + '&find='
+
+        response = requests.get(url+"голень")
+
+        print(response.content)
+
 
 
     def suite(self):
