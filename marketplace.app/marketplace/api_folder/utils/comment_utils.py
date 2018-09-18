466c4536fa88c52f6793999e668e535b6a07044d
Egor Galkin <egor.galkin99@yandex.ru>
2018-09-18 12:40:39 +0300

    ISSUE-323 Added documentation for comment_utils.py

--- marketplace.app/marketplace/api_folder/utils/comment_utils.py
+++ marketplace.app/marketplace/api_folder/utils/comment_utils.py
@@ -5,25 +5,29 @@ from marketplace.api_folder.utils.abortions import abort_if_product_doesnt_exist
 from marketplace.models import Comment, Order
 
 
-def get_comment_by_id(comment_id):
+def get_comment_by_id(comment_id: int) -> Comment:
+    """Returns comment with given id"""
     return abort_if_comment_doesnt_exist_or_get(comment_id)
 
 
-def get_comments_by_product_id(product_id, page=1):
+def get_comments_by_product_id(product_id: int, page: int = 1) -> list:
+    """Returns page of comments for product"""
     abort_if_product_doesnt_exist_or_get(product_id)
     return Comment.query.filter_by(product_id=product_id).order_by(Comment.timestamp.desc()).paginate(page,
                                                                                                       COMMENTS_PER_PAGE,
                                                                                                       False)
 
 
-def get_comments_by_consumer_id(consumer_id, page):
+def get_comments_by_consumer_id(consumer_id: int, page: int = 1) -> list:
+    """Returns page of comments for consumer"""
     abort_if_consumer_doesnt_exist_or_get(consumer_id)
     return Comment.query.filter_by(consumer_id=consumer_id).order_by(Comment.timestamp.desc()).paginate(page,
                                                                                                         COMMENTS_PER_PAGE,
                                                                                                         False)
 
 
-def post_comment(args):
+def post_comment(args: dict) -> Comment:
+    """Create and returns comment by given args"""
     abort_if_product_doesnt_exist_or_get(args['product_id'])
     abort_if_consumer_doesnt_exist_or_get(args['consumer_id'])
     new_comment = comment_schema.load(args).data
@@ -34,7 +38,8 @@ def post_comment(args):
     return new_comment
 
 
-def delete_comment_by_id(comment_id):
+def delete_comment_by_id(comment_id: int) -> dict:
+    """Delete comment with given id"""
     comment = get_comment_by_id(comment_id)
     db.session.delete(comment)
     db.session.commit()
