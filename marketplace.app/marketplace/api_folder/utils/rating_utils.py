401d004d8edd2ac1e94b68f98dfc01bc8ab9cd48
Egor Galkin <egor.galkin99@yandex.ru>
2018-09-18 22:42:52 +0300

    ISSUE-323 Added documentation for rating_utils.py

--- marketplace.app/marketplace/api_folder/utils/rating_utils.py
+++ marketplace.app/marketplace/api_folder/utils/rating_utils.py
@@ -1,8 +1,10 @@
 from marketplace import db
 from marketplace.api_folder.utils.abortions import abort_if_invalid_rating_value
+from marketplace.models import RatedMixin
 
 
-def post_rating(rating, rated):
+def post_rating(rating: int, rated: RatedMixin) -> dict:
+    """Posts rating for given RatedMixin instance"""
     abort_if_invalid_rating_value(rating)
     rating = rated.update_rating(rating)
     db.session.commit()
