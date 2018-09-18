2e2302d033b6a595936dd722ff11e75e8a58bd2c
IrinaZorkova <irinanazorkova@gmail.com>
2018-09-18 12:29:11 +0300

    ISSUE-321 рейтинг для отзывов фронт

--- marketplace.app/marketplace/static/script/review.js
+++ marketplace.app/marketplace/static/script/review.js
@@ -1,10 +1,11 @@
-function makeReviewObject(consumer_name, consumer_id, product_id, body, order_id) {
+function makeReviewObject(consumer_name, consumer_id, product_id, body, order_id, rating) {
     return {
         consumer_name: consumer_name,
         consumer_id: parseInt(consumer_id),
         product_id: parseInt(product_id),
         order_id: order_id,
-        body: body
+        body: body,
+        rating: parseInt(rating)
     };
 }
 
@@ -21,12 +22,20 @@ $('#sendReviews').click(function () {
     let consumer_id = newReviewSection.attr('data-consumer-id');
     let order_id = newReviewSection.attr('data-order-id');
     let number_of_products = $(this).attr('data-number-of-products');
+
     for (let i = 1; i <= number_of_products; i++) {
         let review = $('#review' + i);
         let body = review.val();
         if (body) {
             let product_id = review.attr('data-product-id');
-            let data = makeReviewObject(consumer_name, consumer_id, product_id, body, order_id);
+            for (let k = 1; k < 6; k++) {
+                if ($('#ratingStar' + k + '_' + product_id).is(':checked')) {
+                    var rating = k;
+                    break;
+                }
+
+            }
+            let data = makeReviewObject(consumer_name, consumer_id, product_id, body, order_id, rating);
             postReview(data);
         }
     }
