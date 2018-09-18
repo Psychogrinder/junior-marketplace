a974e09ae7e2c37948763637ae6f65be035284bb
Egor Galkin <egor.galkin99@yandex.ru>
2018-09-18 12:36:14 +0300

    ISSUE-323 Added documentation for checkers

--- marketplace.app/marketplace/api_folder/utils/checkers.py
+++ marketplace.app/marketplace/api_folder/utils/checkers.py
@@ -2,11 +2,13 @@ from marketplace.api_folder.utils import user_utils, producer_utils
 from marketplace.api_folder.utils.abortions import failed_email_uniqueness_check, failed_producer_name_uniqueness_check
 
 
-def check_email_uniqueness(email):
+def check_email_uniqueness(email: str):
+    """Checks if given email is unique"""
     if user_utils.get_user_by_email(email) is not None:
         failed_email_uniqueness_check()
 
 
-def check_producer_name_uniqueness(name):
+def check_producer_name_uniqueness(name: str):
+    """Checks if given producer name is unique"""
     if producer_utils.get_producer_by_name(name) is not None:
         failed_producer_name_uniqueness_check()
