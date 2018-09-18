800f76a5c2573e728e54d7e3d2d4ecdff5a8da58
Egor Galkin <egor.galkin99@yandex.ru>
2018-09-18 22:48:59 +0300

    ISSUE-323 Added documentation for validators.py

--- marketplace.app/marketplace/api_folder/utils/validators.py
+++ marketplace.app/marketplace/api_folder/utils/validators.py
@@ -3,7 +3,11 @@ import re
 from marketplace.api_folder.utils.abortions import failed_email_check, failed_password_len_check
 
 
-def validate_registration_data(email, password):
+def validate_registration_data(email: str, password: str) -> bool:
+    """
+    Checks if email required to email regex
+    Checks password length
+    """
     email_pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
     if not re.match(email_pattern, email):
         failed_email_check(email)
