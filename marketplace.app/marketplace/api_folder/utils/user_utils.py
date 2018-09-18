9d485e71bcbb7d1323a4a74509793ea0066140cf
Egor Galkin <egor.galkin99@yandex.ru>
2018-09-18 22:46:22 +0300

    ISSUE-323 Added documentation for user_utils.py

--- marketplace.app/marketplace/api_folder/utils/user_utils.py
+++ marketplace.app/marketplace/api_folder/utils/user_utils.py
@@ -1,5 +1,6 @@
 from marketplace.models import User
 
 
-def get_user_by_email(email):
+def get_user_by_email(email: str) -> User:
+    """Returns user with given email"""
     return User.query.filter_by(email=email).first()
