6e0ca1791b84a4e9b9db8be2efd1b582f74abf57
Egor Galkin <egor.galkin99@yandex.ru>
2018-09-18 20:10:12 +0300

    ISSUE-323 Added documentation for consumer_utils.py

--- marketplace.app/marketplace/api_folder/utils/consumer_utils.py
+++ marketplace.app/marketplace/api_folder/utils/consumer_utils.py
@@ -7,15 +7,18 @@ from marketplace.api_folder.utils.validators import validate_registration_data
 from marketplace.models import Consumer
 
 
-def get_consumer_by_id(consumer_id):
+def get_consumer_by_id(consumer_id: int) -> Consumer:
+    """Returns consumer with given id"""
     return abort_if_consumer_doesnt_exist_or_get(consumer_id)
 
 
-def get_all_consumers():
+def get_all_consumers() -> list:
+    """Returns list of all consumers"""
     return Consumer.query.filter_by(entity='consumer').all()
 
 
-def post_consumer(args):
+def post_consumer(args: dict):
+    """Post consumer by given args and returns it"""
     validate_registration_data(args['email'], args['password'])
     check_email_uniqueness(args['email'])
     new_consumer = consumer_sign_up_schema.load(args).data
@@ -26,7 +29,8 @@ def post_consumer(args):
     return new_consumer
 
 
-def put_consumer(args, consumer_id):
+def put_consumer(args: dict, consumer_id: int):
+    """Change consumer and returns it"""
     consumer = get_consumer_by_id(consumer_id)
     args['id'] = None
     for k, v in args.items():
@@ -36,13 +40,15 @@ def put_consumer(args, consumer_id):
     return consumer
 
 
-def delete_consumer_by_id(consumer_id):
+def delete_consumer_by_id(consumer_id: int):
+    """Delete consumer with given id"""
     consumer = get_consumer_by_id(consumer_id)
     db.session.delete(consumer)
     db.session.commit()
     return {"message": "Consumer with id {} has been deleted".format(consumer_id)}
 
 
+# Possible deprecated now
 def upload_consumer_image(consumer_id, files):
     consumer = get_consumer_by_id(consumer_id)
     return upload_image(consumer, files)
