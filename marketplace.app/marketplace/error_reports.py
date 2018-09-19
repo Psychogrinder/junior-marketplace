a99fb75fead58ced05fdc1e48a8a76c703b900b4
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-09-19 17:41:52 +0300

    ISSUE-329 добавил контекстменеджер для обработки ошибок influxdb

--- marketplace.app/marketplace/error_reports.py
+++ marketplace.app/marketplace/error_reports.py
@@ -1,3 +1,6 @@
+from contextlib import contextmanager
+import requests
+import influxdb
 try:
     from marketplace import sentry
 except ImportError:
@@ -8,3 +11,13 @@ def send_report(msg, service, **tags):
     if sentry:
         tags.update({'service': service})
         sentry.captureMessage(msg, tags=tags)
+
+
+@contextmanager
+def influx_error_send_report():
+    try:
+        yield
+    except requests.exceptions.ConnectionError:
+        send_report('No connect to influxdb', 'celery', reporter='influxdb')
+    except influxdb.exceptions.InfluxDBClientError as e:
+        send_report(e.message, 'celery', reporter='influxdb')
