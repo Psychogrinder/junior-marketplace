69d16813ace70650737ee06928ff8d5dc915e5f2
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-09-23 21:53:42 +0300

    ISSUE-355 добавил считывание названия доски из инпута

--- marketplace.app/marketplace/static/script/trello_integration.js
+++ marketplace.app/marketplace/static/script/trello_integration.js
@@ -214,33 +214,39 @@ let trelloCardsCreator = function (orders) {
   }
 }
 
-let linkTrelloAccount = function(producer_id) {
-  
-  let onSucces = function(){
-    console.log(Trello.token())
-    fetch(`/api/v1/producers/${producer_id}/trello-link`,{
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
+let linkTrelloAccount = function (producer_id) {
+
+  let inputBoardName = document.getElementsByClassName('trellointegration-input')[0]
+  let trelloContainer = document.getElementsByClassName('trellointegration')[0]
+
+  if (inputBoardName !== 'undefined' && inputBoardName.value.length > 0 && inputBoardName.value.length < 30) {
+
+    let onSucces = function () {
+      fetch(`/api/v1/producers/${producer_id}/trello-link`, {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+          body: JSON.stringify({
+            'trello_token': Trello.token(),
+            'board_name': inputBoardName.value
+          }),
+        })
+        .then(data => trelloContainer.remove())
+    }
+
+    Trello.authorize({
+      type: 'popup',
+      name: 'Marketplace',
+      persist: false,
+      scope: {
+        read: 'true',
+        write: 'true'
       },
-      body:  JSON.stringify({
-        'trello_token': Trello.token(),
-        'board_name': 'Hello'
-      }),
+      expiration: 'never',
+      success: onSucces
     })
-      .then( data => console.log(data.json()))
   }
 
-  Trello.authorize({
-    type: 'popup',
-    name: 'Marketplace',
-    persist: false,
-    scope: {
-      read: 'true',
-      write: 'true'
-    },
-    expiration: 'never',
-    success: onSucces
-  })
 
 }
