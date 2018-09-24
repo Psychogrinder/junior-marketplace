989242325c89c8d95c6bf2b96dd577c01095774c
Roman Melnychuk <jenikuse@gmail.com>
2018-09-24 13:06:20 +0300

    ISSUE-363 Added testing_utils file. Moved to this file login, logout methods. Also created method uniqueEmail(), which needed for registration consumers (based on datetime library)

--- marketplace.app/marketplace/test_selenium/test_authorize.py
+++ marketplace.app/marketplace/test_selenium/test_authorize.py
@@ -1,5 +1,7 @@
 from append_path import *
+from testing_utils import uniqueEmail, login, logout
 import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,60 +9,50 @@ from selenium.webdriver.support import expected_conditions as EC
 from marketplace.models import User
 
 
+
+email_unique = uniqueEmail()
 driver = webdriver.Firefox()
 
-class TestAuthorization(unittest.TestCase):
+
+class TestCaseAuthorization(unittest.TestCase):
 
     def setUp(self):
         self.url = 'http://127.0.0.1:8000'
         self.consumer = User.query.filter_by(entity='consumer').first()
         self.producer = User.query.filter_by(entity='producer').first()
         self.users = [self.consumer, self.producer]
-
-        self.email = "primary@mail.ru"
         self.password = "123123"
 
 
     def test_01_registration(self):
-        url, email, pw, pw2 = self.url, self.email, self.password, self.password
+        url, pw = self.url, self.password
 
         driver.get(url)
         driver.find_element_by_css_selector(".header-login").click()
         driver.find_element_by_css_selector(
             "p.registration-link:nth-child(4) > a:nth-child(1)").click()
-        driver.find_element_by_id("emailRegistration").send_keys(email)
+        driver.find_element_by_id("emailRegistration").send_keys(email_unique)
         driver.find_element_by_id("passwordRegistration").send_keys(pw)
-        driver.find_element_by_id("re_passwordRegistration").send_keys(pw2)
+        driver.find_element_by_id("re_passwordRegistration").send_keys(pw)
 
         driver.find_element_by_id("reg_button").click()
-
+        logout(driver)
 
     def test_02_login(self):
-        url, pw = self.url, self.password
+        pw = self.password
 
-        def login_from_url(url, email):
-            driver.get(url)
-            driver.find_element_by_xpath("/html/body/header/nav/div/div/a").click()
-            driver.find_element_by_id("emailAuthorisation").send_keys(email)
-            driver.find_element_by_id("passwordAuthorisation").send_keys(pw)
-            driver.find_element_by_id("authButton").click()
-
-        def logout():
-            driver.find_element_by_css_selector("button.btn:nth-child(1)").click()
-            driver.find_element_by_id("logoutButton").click()
-
-        login_from_url(url, self.consumer.email)
-        logout()
-        login_from_url(url, self.producer.email)
+        login(driver, self.consumer.email, pw)
+        logout(driver)
+        login(driver, self.producer.email, pw)
 
 
     def test_03_logout(self):
-        driver.find_element_by_css_selector("button.btn:nth-child(1)").click()
-        driver.find_element_by_id("logoutButton").click()
+        logout(driver)
 
 
     # def tearDown(self):
-    #     driver.close()
+    #     self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
+    driver.close()
