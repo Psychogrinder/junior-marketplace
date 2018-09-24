7bf26f8049fb726753ddbf8b1b4d5943f1e945a4
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-09-24 15:58:13 +0300

    ISSUE-355 удалил принты

--- marketplace.app/marketplace/api_folder/trello_api.py
+++ marketplace.app/marketplace/api_folder/trello_api.py
@@ -1,9 +1,14 @@
 from flask_restful import Resource
 from flask import request
+from marketplace.error_reports import send_report
+from marketplace.trello_integrations import change_order_status
 
 
 class TrelloWebHook(Resource):
 
     def post(self):
-        print(request.json())
+        change_order_status(request.json)
+        return {}, 200
+
+    def head(self):
         return {}, 200
