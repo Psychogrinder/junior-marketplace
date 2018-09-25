0058c6b6bba5b0ce3851c7e5a7fe18a8ddf8efcb
Roman Melnychuk <jenikuse@gmail.com>
2018-09-25 14:03:32 +0300

    ISSUE-380 Moved test edit consumer to the separate file. Added asserts to verificate that saved values correspondents loading data

---
+++ marketplace.app/marketplace/test_selenium/test_consumer.py
@@ -0,0 +1,63 @@
+from append_path import *
+
+from testing_utils import login, logout, getPhoneMask, getEditElements, setDictValues, \
+    getCurrentDataFromElements
+import unittest
+from selenium import webdriver
+from marketplace.models import Category, User
+
+driver = webdriver.Firefox()
+
+
+class TestConsumer(unittest.TestCase):
+
+    def setUp(self):
+        self.url = 'http://127.0.0.1:8000'
+        self.consumer = User.query.filter_by(entity='consumer').order_by(User.id.desc()).first()
+        self.producer = User.query.filter_by(entity='producer').order_by(User.id.desc()).first()
+        self.users = [self.consumer, self.producer]
+        self.password = "123123"
+
+        self.load_data = {"first_name": "",
+                          "last_name": "",
+                          "patronymic": "",
+                          "phone": "",
+                          "address": "",
+                          }
+
+
+    def test_01_edit_consumer(self):
+        login(driver, self.consumer.email, self.password)
+        driver.find_element_by_css_selector("button.btn:nth-child(1)").click() # User menu btn
+        driver.find_element_by_css_selector("a.dropdown-item:nth-child(1)").click() # Profile btn
+        driver.find_element_by_css_selector(".edit-profile > a:nth-child(1)").click()  # Edit profile btn
+
+        elements_to_edit = getEditElements(driver)
+        data_to_edit = getCurrentDataFromElements(elements_to_edit)
+        save_profile = driver.find_element_by_id("save_consumer_profile")
+        delete_profile = driver.find_element_by_css_selector(".out-of-stock > a:nth-child(1)") # Delete profile btn
+
+        # filling new data in the fields on the Edit User Page
+        load_data = setDictValues(self.load_data)
+        keys = list(load_data.keys())
+        for arg in range(len(data_to_edit)):
+            key = keys[arg]
+            elements_to_edit[arg].clear()
+            elements_to_edit[arg].send_keys(load_data[key])
+        edited_data = getCurrentDataFromElements(elements_to_edit)
+        save_profile.click()
+        driver.find_element_by_xpath("/html/body/main/div[1]/div/p/a").click() # Edit profile btn
+
+        # verification that saved values corresponds to loading data
+        for arg in range(len(edited_data)):
+            key = keys[arg]
+            if key == "phone":
+                phone_number = getPhoneMask(load_data[key])  # in load data string of nums
+                self.assertEqual(phone_number, edited_data[arg])
+            else:
+                self.assertEqual(load_data[key], edited_data[arg])
+
+
+    # def tearDown(self):
+    #     self.driver.close()
+
--- marketplace.app/marketplace/test_selenium/test_products.py
+++ marketplace.app/marketplace/test_selenium/test_products.py
@@ -1,6 +1,6 @@
 from append_path import *
 
-from testing_utils import login, logout, getEditArgs
+from testing_utils import login, logout
 import unittest
 from random import randint, choice
 from selenium import webdriver
@@ -9,8 +9,7 @@ from selenium.common import exceptions as ex
 
 driver = webdriver.Firefox()
 
-
-class Order(unittest.TestCase):
+class TestOrder(unittest.TestCase):
 
     def setUp(self):
         self.url = 'http://127.0.0.1:8000'
@@ -19,7 +18,7 @@ class Order(unittest.TestCase):
         self.users = [self.consumer, self.producer]
         self.password = "123123"
 
-        self.edit_data = {"first_name": "",
+        self.load_data = {"first_name": "",
                           "last_name": "",
                           "patronymic": "",
                           "phone": "",
@@ -53,40 +52,6 @@ class Order(unittest.TestCase):
     #
     #     driver.close()
 
-    def test_03_edit_consumer(self):
-        login(driver, self.consumer.email, self.password)
-        driver.find_element_by_css_selector("button.btn:nth-child(1)").click() # User menu btn
-        driver.find_element_by_css_selector("a.dropdown-item:nth-child(1)").click() # Profile btn
-        driver.find_element_by_css_selector(".edit-profile > a:nth-child(1)").click()  # Edit profile btn
-
-        elements, edited = getEditArgs(driver)
-
-        save_profile = driver.find_element_by_id("save_consumer_profile")
-        delete_profile = driver.find_element_by_css_selector(".out-of-stock > a:nth-child(1)") # Delete profile btn
-
-        edit_data = self.edit_data
-        edit_data["first_name"] = "Rama1"
-        edit_data["last_name"] = "lava1"
-        edit_data["patronymic"] = "bi1"
-        edit_data["phone"] = "9991112233"
-        edit_data["address"] = "fake str 55"
-
-        keys = list(edit_data.keys())
-        for arg in range(len(elements)):
-            key = keys[arg]
-            new_value = self.edit_data[key]
-            elements[arg].clear()
-            elements[arg].send_keys(new_value)
-
-        save_profile.click()
-        driver.find_element_by_xpath("/html/body/main/div[1]/div/p/a").click()
-
-        print(edited)
-        print(edit_data)
-        for arg in range(len(edited)):
-            key = keys[arg]
-            edited, to_edit = edited[arg], edit_data[key]
-            print(to_edit, edited)
 
 
 
