3b00a848cfcc5941bce17efa4003e5f9abd4d78a
Alexey Yunoshev <33806564+alexey-yunoshev@users.noreply.github.com>
2018-09-26 12:45:29 +0300

    Update config.py

--- marketplace.app/marketplace/config.py
+++ marketplace.app/marketplace/config.py
@@ -43,7 +43,7 @@ class Base(object):
     INFLUXDB_DATABASE = 'marketplace'
 
     TRELLO_API_KEY = '15ff34e4152f835730b1ad59f830448a'
-    TRELLO_BOARD_LISTS = ['Не обработан', 'Обрабатывается', 'Отправлен', 'Готов к самовывозу', 'Завершен']
+    TRELLO_BOARD_LISTS = ['Не обработан', 'Обрабатывается', 'Отправлен', 'Готов к самовывозу', 'Завершён']
 
 
 class Development(Base):
