86b42d1367dbd459a0450971407096b4e127491e
Egor Galkin <egor.galkin99@yandex.ru>
2018-09-26 15:15:22 +0300

    ISSUE-334 Update generate_sitemap

--- marketplace.app/marketplace/sitemap_tools.py
+++ marketplace.app/marketplace/sitemap_tools.py
@@ -6,26 +6,25 @@ from marketplace import app, SITE_DOMAIN, celery
 from marketplace.api_folder.utils import product_utils, producer_utils
 
 
-@celery.task
+@celery.task(name='sitemap_tools.generate_sitemap')
 def generate_sitemap():
-    print('Start async sitemap ge')
     pages = []
-    ten_days_ago = datetime.now() - timedelta(days=10)
+    cur_date = datetime.now()
     for rule in app.url_map.iter_rules():
         if "GET" in rule.methods:
             if not str(rule).startswith('/api/v1/'):
                 if len(rule.arguments) == 0:
                     pages.append(
-                        ['{}{}'.format(SITE_DOMAIN, rule), ten_days_ago]
+                        ['{}{}'.format(SITE_DOMAIN, rule), cur_date]
                     )
                 if str(rule) == '/producer/<int:producer_id>':
                     producers = producer_utils.get_all_producers()
                     for producer in producers:
-                        pages.append(['{}/producer/{}'.format(SITE_DOMAIN, producer.id), ten_days_ago])
+                        pages.append(['{}/producer/{}'.format(SITE_DOMAIN, producer.id), cur_date])
                 if str(rule) == '/products/<int:product_id>':
                     products = product_utils.get_all_products()
                     for product in products:
-                        pages.append(['{}/products/{}'.format(SITE_DOMAIN, product.id), ten_days_ago])
+                        pages.append(['{}/products/{}'.format(SITE_DOMAIN, product.id), cur_date])
     sitemap_xml = render_template('sitemap.xml', pages=pages)
     with open('sitemap.xml', 'w+') as sitemap:
         sitemap.write(sitemap_xml)
