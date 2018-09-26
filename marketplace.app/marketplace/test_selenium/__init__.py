d9ff8eb0ae3ee142e2a6f165707606c198afb422
Roman Melnychuk <jenikuse@gmail.com>
2018-09-26 15:42:28 +0300

    ISSUE-384 added into test suite producer cases

--- marketplace.app/marketplace/test_selenium/__init__.py
+++ marketplace.app/marketplace/test_selenium/__init__.py
@@ -1,12 +1,17 @@
 from test_authorize import Authorization
-from test_products import TestOrder
+from test_products import TestProducts
 from test_consumer import TestConsumer
+from test_producer import TestProducer
 import unittest
 
-auth = unittest.TestLoader().loadTestsFromTestCase(Authorization)
-order = unittest.TestLoader().loadTestsFromTestCase(TestOrder)
-consumer = unittest.TestLoader().loadTestsFromTestCase(TestConsumer)
 
-test_suite = unittest.TestSuite([auth, order, consumer])
+auth = unittest.TestLoader().loadTestsFromTestCase(Authorization)
+order = unittest.TestLoader().loadTestsFromTestCase(TestProducts)
+consumer = unittest.TestLoader().loadTestsFromTestCase(TestConsumer)
+producer = unittest.TestLoader().loadTestsFromTestCase(TestProducer)
+
+
+test_suite = unittest.TestSuite([auth, order, consumer, producer])
+
 
 unittest.TextTestRunner(verbosity=2).run(test_suite)
