5bff850df82d554f28d1bf7662ade3de59b11b2a
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-09-28 15:09:37 +0300

    ISSUE-403 добавил создание volume marketplace_chat

--- marketplace.app/deploy-prod.sh
+++ marketplace.app/deploy-prod.sh
@@ -5,6 +5,8 @@ source ../.env.prod
 
 docker volume create marketplace_user_image
 docker volume create marketplace_tasks
+docker volume create marketplace_chat
+
 docker network create -d bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 marketplace
 
 docker-compose -f docker-compose-prod.yml up --build $1
