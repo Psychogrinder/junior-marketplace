58e216cc0a00f7ab4680fee5db94748a1e2286f1
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-10-02 15:27:18 +0300

    ISSUE-408 добавил в имя дампфайла время

--- marketplace.db/db_dump.sh
+++ marketplace.db/db_dump.sh
@@ -6,4 +6,4 @@ source ../DOCKER_ENV_PRODUCTION
 
 docker exec marketplace.db \
     pg_dump -U postgres marketplace.db | \
-    gzip > marketplace_db._$(date "+%Y-%m-%d").sql.gz
+    gzip > marketplace_db._$(date "+%Y-%m-%d__%H-%M").sql.gz
