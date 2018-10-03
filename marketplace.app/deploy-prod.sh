8842e8f70ecf90eae5efbe575f8f38c6b77d6391
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-10-03 16:36:31 +0300

    ISSUE-408 добавил 'set -e'

--- marketplace.app/deploy-prod.sh
+++ marketplace.app/deploy-prod.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -e
 
 source ../DOCKER_ENV_PRODUCTION
 source ../.env.prod
 
+docker network create -d bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 marketplace && true
 docker volume create marketplace_user_image
 docker volume create marketplace_tasks
-docker network create -d bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 marketplace
+
 
 docker-compose -f docker-compose-prod.yml up --build $1
