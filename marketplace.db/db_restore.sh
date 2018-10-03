11b8533c0b60e9077bbfa0fbec30d7405e84e634
Kalinin Kirill <kalinin-kirill@mail.ru>
2018-10-03 14:14:19 +0300

    ISSUE-408 изменил параметр команды set. убрал '&& \'

--- marketplace.db/db_restore.sh
+++ marketplace.db/db_restore.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -x
+set -ex
 
 source ../DOCKER_ENV_PRODUCTION
 
 if [[ $2 == "create" ]]
 then
-  docker exec marketplace.db psql -c 'drop database "marketplace.db";' -U postgres && \
+  docker exec marketplace.db psql -c 'drop database "marketplace.db";' -U postgres
   docker exec marketplace.db psql -c 'create database "marketplace.db";' -U postgres
 fi
 
